9cceb1b511149a4768323f78cf5b34dcc3573844
Mopuri Babavali <gprecbabavali981@gmail.com>
2024-11-09 08:25:04 +0530

    Puah

---
+++ Lab2.py
@@ -0,0 +1,7 @@
+age = 25
+# The interpreter automatically determines the type of the variable:
+print(type(age))  # Output: <class 'int'>
+
+# You can also reassign a variable to a different type without any issues:
+age = "twenty-five"
+print(type(age))  # Output: <class 'str'>
